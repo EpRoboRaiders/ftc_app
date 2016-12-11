1e3866298dd38a0e5a67f5fd344ea9c3ed070b4e
EpRoboRaiders <eproboraiders@gmail.com>
2016-12-11 17:45:31 -0600

    Added code to make the Color Sensor show up in teleop for testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRI_ColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MRI_ColorSensor.java
@@ -16,41 +16,29 @@ Support is available by emailing support@modernroboticsinc.com.
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 import android.graphics.Color;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.*;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-@Autonomous(name = "Color Sensor", group = "Concept")
-@Disabled
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Color Sensor", group = "MRI")
 public class MRI_ColorSensor extends LinearOpMode {
-    static final double MAX_POS = 0.5;     // Maximum rotational position
-    static final double MIN_POS = 0.1;     // Minimum rotational position
-    static final double INCREMENT = 0.01;
-    com.qualcomm.robotcore.hardware.Servo Rservo;
-    com.qualcomm.robotcore.hardware.Servo Lservo;
+
     ColorSensor colorSensor;       //Instance of ColorSensor - for reading color
-    // TouchSensor touch;             //Instance of TouchSensor - for changing color sensor mode
+    TouchSensor touch;             //Instance of TouchSensor - for changing color sensor mode
     DeviceInterfaceModule CDI;     //Instance of DeviceInterfaceModule - for showing a red or blue LED
-    double rightposition = MAX_POS;
-    double leftposition = MAX_POS;
-    private ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
 
     @Override
     public void runOpMode() throws InterruptedException {
         //the below three lines set up the configuration file
         colorSensor = hardwareMap.colorSensor.get("color");
-        //touch = hardwareMap.touchSensor.get("t");
+        touch = hardwareMap.touchSensor.get("t");
         //We named the CDI using the default name given by the FTC configuration file
         CDI = hardwareMap.deviceInterfaceModule.get("Device Interface Module 1");
-        Rservo = hardwareMap.servo.get("right claw");
-        Lservo = hardwareMap.servo.get("left claw");
+
+
         boolean touchState = false;  //Tracks the last known state of the touch sensor
         boolean LEDState = true;     //Tracks the mode of the color sensor; Active = true, Passive = false
 
@@ -60,43 +48,47 @@ public class MRI_ColorSensor extends LinearOpMode {
         //Active - For measuring reflected light. Cancels out ambient light
         //Passive - For measuring ambient light, eg. the FTC Color Beacon
 
+        float hsvValues[] = {0, 0, 0};  //used to get Hue
 
         while (opModeIsActive()) {  //Main loop of program
-            Lservo.setPosition(leftposition);
-            Rservo.setPosition(rightposition);
+
+            //The below two if() statements ensure that the mode of the color sensor is changed only once each time the touch sensor is pressed.
+            //The mode of the color sensor is saved to the sensor's long term memory. Just like flash drives, the long term memory has a life time in the 10s or 100s of thousands of cycles.
+            //This seems like a lot but if your program wrote to the long term memory every time though the main loop, it would shorten the life of your sensor.
+
+            if (!touchState && touch.isPressed()) {  //If the touch sensor is just now being pressed (was not pressed last time through the loop but now is)
+                touchState = true;                   //Change touch state to true because the touch sensor is now pressed
+                LEDState = !LEDState;                //Change the LEDState to the opposite of what it was
+                colorSensor.enableLed(LEDState);     //Set the mode of the color sensor using LEDState
+            }
+            if (!touch.isPressed()) {                //If the touch sensor is now pressed
+                touchState = false;                  //Set the touchState to false to indicate that the touch sensor was released
+            }
+
+            //calculate hue
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
 
             //display values
+            telemetry.addData("2 Clear", colorSensor.alpha());
             telemetry.addData("3 Red  ", colorSensor.red());
             telemetry.addData("4 Green", colorSensor.green());
             telemetry.addData("5 Blue ", colorSensor.blue());
+            telemetry.addData("6 Hue", hsvValues[0]);
+            telemetry.addData("7 Touch", touch.isPressed());
 
             //illuminate the RED/BLUE LED on the Core Device Interface if the RED/BLUE value is greatest
-            runtime.reset();
-            while (colorSensor.red() > colorSensor.blue() && colorSensor.red() > colorSensor.green() && (runtime.seconds() <= 1)) {
-                telemetry.addData("1", "Waiting %2.5f S Elapsed", runtime.seconds());
-                telemetry.update();
-            }
-            while (colorSensor.red() > colorSensor.blue() && colorSensor.red() > colorSensor.green()) {
-                Lservo.setPosition(leftposition);
-                Rservo.setPosition(rightposition);
-
+            if (colorSensor.red() > colorSensor.blue() && colorSensor.red() > colorSensor.green()) {
                 CDI.setLED(1, true);           //Red ON
                 CDI.setLED(0, false);          //Blue OFF
-            }
-            runtime.reset();
-            while (colorSensor.blue() > colorSensor.red() && colorSensor.blue() > colorSensor.green() && (runtime.seconds() <= 1)) {
-                telemetry.addData("2", "Waiting %2.5f S Elapsed", runtime.seconds());
-                telemetry.update();
-            }
-            while (colorSensor.blue() > colorSensor.red() && colorSensor.blue() > colorSensor.green()) {
-                Lservo.setPosition(leftposition);
-                Rservo.setPosition(rightposition);
-
+            } else if (colorSensor.blue() > colorSensor.red() && colorSensor.blue() > colorSensor.green()) {
                 CDI.setLED(1, false);          //Red OFF
                 CDI.setLED(0, true);           //Blue ON
+            } else {
+                CDI.setLED(1, false);           //Red OFF
+                CDI.setLED(0, false);           //Blue OFF
             }
 
-        }
-        waitOneFullHardwareCycle();  //wait for all new data to go from the phone to the controllers and from the controllers to the phone.
-    } //End main loop of program
+            waitOneFullHardwareCycle();  //wait for all new data to go from the phone to the controllers and from the controllers to the phone.
+        } //End main loop of program
+    }
 }
